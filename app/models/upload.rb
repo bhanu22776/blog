dbeb9d454af512ad7257f875a19645317440be8d
bhanuprasad <bhanuprasadmende@gmail.com>
2015-06-25 14:03:07 +0530

    paperclip files

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -1,11 +1,26 @@
 	class Upload < ActiveRecord::Base
-	attr_accessible :name, :photo
+	attr_accessible :name, :photo, :document, :audio, :video
 
 	has_attached_file :photo, :styles => { :small => "150x150>" },
 	              :url  => "/assets/uploads/:id/:style/:basename.:extension",
 	              :path => ":rails_root/public/assets/uploads/:id/:style/:basename.:extension"
 
-	validates_attachment_presence :photo
+	# validates_attachment_presence :photo
 	validates_attachment_size :photo, :less_than => 5.megabytes
 	validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/png']
+
+
+
+	has_attached_file :document, styles: {thumbnail: "60x60#"}
+	validates_attachment :document, content_type: { content_type: "application/pdf" }
+
+
+	has_attached_file :audio
+	validates_attachment_content_type :audio, :content_type => [ 'audio/mp3','audio/mpeg'],
+                     :file_name => { :matches => [/mp3\Z/] }
+
+	has_attached_file :video
+	validates_attachment_content_type :video, :content_type => [ 'video/mp4', 'video.mov', 'video/mpeg','video/mpeg4']
+
 end
+
